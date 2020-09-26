5981d48565032d6f39b1a81912f01f645121dc6b
durden666 <thanks.vlad12@gmail.com>
2020-09-26 14:32:22 +0300

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let matches = str.members(/\b(\w)/g); 
-  let acronym = members.join('');
-
-      
-
-
+  if (!Array.isArray(members)) return false;
   
-};
+    let teamArr = [];
+
+    if (Array.isArray(members)) {
+      members.forEach((item)=> {
+    if (typeof(item) =='string' ) {
+      teamArr.push(item.trim().charAt(0).toUpperCase());
+      }
+    })
+  } return teamArr.sort().join('');
+}
 
 
