af83ca088dc15cdba9510b195dc5bccdafed49ac
durden666 <thanks.vlad12@gmail.com>
2020-10-04 23:31:24 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,36 @@
 const CustomError = require("../extensions/custom-error");
+const { arr } = require("./simple-chain");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw Error
+
+  let newArr = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      if (arr[i + 2] === '--double-prev' || arr[i + 2] === '--discard-prev')
+      i++;
+      i++;
+    } 
+    else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+      if ((arr[i - 1]) !== undefined) {
+        newArr.pop()
+      } 
+    }
+    else if (arr[i] === '--double-next') {
+      if ((arr[i + 1]) !== undefined) {
+        newArr.push(arr[i + 1])
+      }
+    }
+    else if (arr[i] === '--double-prev'  && arr[i - 2] !== '--discard-next') {
+      if ((arr[i - 1]) !== undefined) {
+        newArr.push(arr[i - 1])
+      }
+    }
+     else {
+        newArr.push(arr[i]);
+    }
+  }
+    return newArr;
+}
