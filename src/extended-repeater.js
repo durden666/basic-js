a188fa2e2eb216d2bf89769287ee598d1cecf1fa
durden666 <thanks.vlad12@gmail.com>
2020-11-01 12:45:40 +0300

    feat:add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  str = String(str);
+
+  if (!options.additionRepeatTimes) {
+    options.additionRepeatTimes = 1 
+  } else {
+    options.additionRepeatTimes;
+  }
+  // 
+  if (!options.repeatTimes) {
+    options.repeatTimes = 1
+  } else {
+    options.repeatTimes;
+  }
+  // 
+  if (!options.separator ) {
+    options.separator = '+'
+  } else {
+    options.separator;
+  }
+  // 
+  if (!options.additionSeparator) {
+    options.additionSeparator = '|'
+  } else {
+    options.additionSeparator;
+  }
+  // 
+  if (options.addition || options.addition === false || options.addition === null) {
+    String(options.addition)
+  } else {
+    options.addition = '';
+  }
+  // 
+  let substring = `${options.addition}${options.additionSeparator}`.repeat(options.additionRepeatTimes).slice(0, -`${options.additionSeparator.length}`);
+  // 
+  let string = `${str}${substring}${options.separator}`.repeat(options.repeatTimes).slice(0, -`${options.separator.length}`);
+
+  return string;
 };
-  
